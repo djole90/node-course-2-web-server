d693c472add74d4ba6011ffd2bb81676b0235b87
Djole90 <djole123@gmail.com>
2019-02-23 12:10:14 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const hbs = require('hbs')
+const fs = require('fs')
+
+const app = express()
+
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs')
+
+
+app.use((req, res, next) => {
+    const now = new Date().toString()
+    const log = `${now} ${req.method} ${req.url}`
+    console.log(log)
+    fs.appendFile('server.log', log + '\n', err => {
+       if (err) {
+           console.log('unable to append to server.log')
+       }
+    })
+    next()
+})
+
+app.use((req, res, next) => {
+    res.render('maintance.hbs')
+})
+
+app.use(express.static(__dirname + '/public'))
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear())
+hbs.registerHelper('screamIt', text => text.toUpperCase())
+
+app.get('/', (req, res, next) => {
+    res.render('home.hbs', {
+        pageTitle: 'Homepage',
+        currentYear: new Date().getFullYear(),
+        welcomeMessage: 'Jebo ga responza evo ga'
+    })
+})
+
+app.get('/about', (req, res, next) => {
+    res.render('about.hbs', {
+        pageTitle: 'About page',
+        currentYear: new Date().getFullYear(),
+        welcomeMessage: 'Govno'
+       
+    })
+})
+
+app.get('/bad', (req, res, next) => {
+    res.send({
+        errorMessage: 'Jebo ga evo i ERRORRRA'
+    })
+})
+
+app.listen(3000)
